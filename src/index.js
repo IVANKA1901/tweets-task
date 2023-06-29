667ace46903ce81f00c718c302223aec4f416c49
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-06-29 18:05:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
-      <BrowserRouter>
+      <BrowserRouter basename="/tweets-task">
         <App />
       </BrowserRouter>
     </PersistGate>
