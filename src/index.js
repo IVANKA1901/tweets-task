9d3007b2b27485deb800391a16376d8aec499e1d
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-06-29 15:52:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -17,3 +18,4 @@ root.render(
     </PersistGate>
   </Provider>
 );
+reportWebVitals();
